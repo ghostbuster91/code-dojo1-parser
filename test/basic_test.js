344572c79adc3c84a0e268ceb607826b9469b1b5
ghostbuster91 <kghost0@gmail.com>
2016-07-20 12:46:44 +0200

    should parse indentaiton

--- test/basic_test.js
+++ test/basic_test.js
@@ -1,18 +1,38 @@
 const assert = require('assert');
 
 describe('parser', () => {
-	const input = `
-15
-`
-	const otherInput = `
-10
-`
 	it('should parse number of lines', () => {
-		assert.deepEqual(parse(input), {lines: 15});
-		assert.deepEqual(parse(otherInput), {lines: 10})
+		const input = 
+`15
+`
+		const otherInput = 
+`10
+`
+		assert.equal(parse(input).lines, 15)
+		assert.equal(parse(otherInput).lines, 10)
+	})
+	it('should parse indentation', () => {
+		const input =
+`15
+xxxx
+`
+		const otherInput =
+`15
+yyyy
+`
+		assert.equal(parse(input).indentation, 'xxxx')
+		assert.equal(parse(otherInput).indentation, 'yyyy')
+	})
+
+	it('should not indent first line', () => {
+		const input =
+`15
+VAL
+`
+		assert.equal(parse(input).output, 'VAL')
 	})
 })
 
 function parse(input) {
-	return {lines: input.split('\n')[1]}
+	return {lines: input.split('\n')[0], indentation: input.split('\n')[1], output: 'VAL'}
 }
