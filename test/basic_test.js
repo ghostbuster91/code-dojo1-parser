6536b2c3806faade967ffb7b5ae4b0f32f413846
ghostbuster91 <kghost0@gmail.com>
2016-07-20 12:28:11 +0200

    should parse number of lines

--- test/basic_test.js
+++ test/basic_test.js
@@ -1,7 +1,18 @@
 const assert = require('assert');
 
-describe('test', () => {
-	it('test', () => {
-		assert(true);
+describe('parser', () => {
+	const input = `
+15
+`
+	const otherInput = `
+10
+`
+	it('should parse number of lines', () => {
+		assert.deepEqual(parse(input), {lines: 15});
+		assert.deepEqual(parse(otherInput), {lines: 10})
 	})
 })
+
+function parse(input) {
+	return {lines: input.split('\n')[1]}
+}
