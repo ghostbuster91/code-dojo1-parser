512b331f7a893acef9b91da4ae8239945c22e58b
ghostbuster91 <kghost0@gmail.com>
2016-07-21 10:38:51 +0200

    should indent if statement body

--- test/basic_test.js
+++ test/basic_test.js
@@ -44,10 +44,47 @@ VAR
 `
 		assert.equal(parse(input).output, expected)
 	})
+	it('should indent if statement body', () => {
+		const input = 
+`15
+xxxx
+	IF !(I MOD 3) THEN
+  PRINT "FIZZ"
+  		ENDIF`
+		const expected = 
+`IF !(I MOD 3) THEN
+xxxxPRINT "FIZZ"
+ENDIF`
+		assert.equal(parse(input).output, expected)
+	})
 })
 
 function parse(input) {
 	const lines = input.split('\n')
-	const output = lines.slice(2).map((elem) => elem.trim()).join('\n')
-	return {lines: lines[0], indentation: lines[1], output: output}
+	const output = lines.slice(2).map((elem) => elem.trim())
+	const indentation = lines[1]
+	var indentationCount = 0
+	const indentedOutout = output.map((elem, index)=> {
+		if(elem.contains('ENDIF')){
+			indentationCount--
+		}
+		const indentedLine = range(indentationCount).map((elem)=> indentation).join('') + elem
+
+		if(elem.contains('IF')){
+			indentationCount++
+		} 
+		return indentedLine
+		}).join('\n')
+
+	return {lines: lines[0], indentation: indentation, output: indentedOutout}
 }
+
+function range(end){
+	const output = []
+	for(i =0; i < end; i++ ){
+		output.push(i)
+	}
+	return output
+}
+
+String.prototype.contains = function(it) { return this.indexOf(it) != -1 }
